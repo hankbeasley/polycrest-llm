2008a809066abf26043a68a7cb56bad90b79b6d9
Hank Beasley <hank.beasley@durwella.com>
2025-02-16 08:39:13 -0600

    chore: update model ID to DeepSeek-R1-Distill-Qwen-7B in predownload script

--- src/predownload.py
+++ src/predownload.py
@@ -19,8 +19,8 @@ from typing import List, Dict, Any
 from transformers import PreTrainedTokenizerBase
 if __name__ == "__main__":
     print("Starting...")
-    model_id = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
-    tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
+    model_id = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
         device_map="auto",  
